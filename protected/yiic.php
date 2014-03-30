852433e54f76b6418fb12ef7def9ea9d74ac5c72
queenblight <queenblight@gmail.com>
2014-03-30 23:58:02 +0200

    first commit litest

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic='C:\Users\Pisnici\Downloads\yii-1.1.14.f0fee9\yii-1.1.14.f0fee9\framework\yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
